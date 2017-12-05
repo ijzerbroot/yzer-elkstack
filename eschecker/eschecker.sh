2e7d1196b4860cb5449f0da5df8ab9814a8e4453
Frank Hoeben <frank@lift.cards>
2017-12-05 11:48:03 +0100

    metricbeat and filebeat added

--- eschecker/eschecker.sh
+++ eschecker/eschecker.sh
@@ -7,6 +7,7 @@ do
     bash /escheckerchecks/${script} >> /eschecker/web/metrics/index.html.tmp
   done
   cp -p /eschecker/web/metrics/index.html.tmp /eschecker/web/metrics/index.html
+  echo "diskusageperc|/|`df -h / |grep % | awk '{ print $(NF-1) }' | tail -1 | sed 's/%//'`"
   echo "eschecker sleeping for ${SLEEPTIME} seconds"
   sleep ${SLEEPTIME}
 done
