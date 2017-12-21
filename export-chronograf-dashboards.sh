9e3bf2165181a06475b3c0f26fe7484bbc798b0a
Frank Hoeben <frank@lift.cards>
2017-12-21 15:19:59 +0100

    instructions on importing and exporting dashboards

--- export-chronograf-dashboards.sh
+++ export-chronograf-dashboards.sh
@@ -1,3 +1,8 @@
 SRC=http://172.31.200.50:8888/chronograf/v1/dashboards
 TEL=1
 curl -Ss $SRC|jq -r '.dashboards[]|@json' |while IFS= read -r dashboard; do echo $dashboard > dash.$TEL ;TEL=`expr $TEL + 1` ; done
+
+
+import with:
+
+ curl -X POST -H "Accept: application/json" -d @file DEST_URL
