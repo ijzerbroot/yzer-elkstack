2aa6b74045ec3fc998d668aa3e92907a5d6d6284
Frank Hoeben <frank@lift.cards>
2017-12-21 15:50:54 +0100

    added logtrail config but directory may not yet exist before plugin-install...

--- kibana/setup.sh
+++ kibana/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/bin/kibana-plugin install https://github.com/sivasamyk/logtrail/releases/download/v0.1.23/logtrail-6.0.0-0.1.23.zip
+/usr/share/kibana/bin/kibana-plugin install https://github.com/sivasamyk/logtrail/releases/download/v0.1.23/logtrail-6.0.0-0.1.23.zip
 /usr/local/bin/kibana-docker
