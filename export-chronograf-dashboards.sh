84bd3ba4233ded864f504936a5a51e0f4d437001
Frank Hoeben <frank@lift.cards>
2017-12-21 14:50:18 +0100

    added telegraf dashboards and instructions

---
+++ export-chronograf-dashboards.sh
@@ -0,0 +1,3 @@
+SRC=http://172.31.200.50:8888/chronograf/v1/dashboards
+TEL=1
+curl -Ss $SRC|jq -r '.dashboards[]|@json' |while IFS= read -r dashboard; do echo $dashboard > dash.$TEL ;TEL=`expr $TEL + 1` ; done
