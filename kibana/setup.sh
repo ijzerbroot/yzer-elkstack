6e143362b40f408a0d5ba0c169fba39f544b42c6
Frank Hoeben <frank@lift.cards>
2018-01-14 08:31:11 +0100

    Kibana back to local

--- kibana/setup.sh
+++ kibana/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/share/kibana/bin/kibana-plugin install https://github.com/sivasamyk/logtrail/releases/download/v0.1.24/logtrail-6.1.1-0.1.24.zip
+/usr/share/kibana/bin/kibana-plugin install https://github.com/sivasamyk/logtrail/releases/download/v0.1.25/logtrail-6.1.1-0.1.25.zip
 /usr/share/kibana/bin/kibana-plugin remove x-pack
 cp /tmp/logtrailconf /usr/share/kibana/plugins/logtrail/logtrail.json
 /usr/local/bin/kibana-docker
