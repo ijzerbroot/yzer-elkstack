83588d3199d9deb450ae1e24cacc434014647a28
Frank Hoeben <frank@lift.cards>
2018-01-18 13:51:39 +0100

    e

--- base/scope.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-sudo curl -L git.io/scope -o /usr/local/bin/scope
-sudo chmod a+x /usr/local/bin/scope
-sudo scope launch 172.31.200.51 172.31.200.52 172.31.200.53
-
-sudo docker update --restart=always weavescope
